d99187d3927c982c415e24b6cd22cb0ec99399fb
mohammed quneifdi <alladqanymhmd@gmail.com>
2022-07-09 01:36:56 +0300

    its done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   const handleState = () => setToggle(!toggle);
 
   return (
-    <div className='nav-bar'>
+    <div className={toggle ? 'nav-bar active' : 'nav-bar inactive'}>
     <div className="container">
       <div className="nav-section">
         <div className="logo">
@@ -20,7 +20,7 @@ function Navbar() {
          {
            toggle ? 
           <span className='close' onClick={handleState}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="19">
               <g fill="#2D314D" fillRule="evenodd">
                 <path d="M.868.661l16.97 16.97-.706.708L.162 1.369z"/>
                 <path d="M.161 17.632L17.131.662l.708.706-16.97 16.97z"/>
@@ -29,7 +29,7 @@ function Navbar() {
           </span>
           :
             <span className='burger' onClick={handleState}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15">
               <g fill="#2D314D" fillRule="evenodd">
                 <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z"/>
                 </g>
@@ -37,7 +37,6 @@ function Navbar() {
           </span>
           }
         </div>
-        <div className="overla"></div>
         <ul className={toggle ? "list active" : "list"}>
           <li>Home</li>
           <li>About</li>
